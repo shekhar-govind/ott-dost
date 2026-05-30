9ee818935b6f2af6a3c4212d3a357289412c7677
shekhar govind <shekhargovind@shekhars-MacBook-Pro.local>
2026-05-30 15:35:15 +0530

    Revert "Reset mobile Chrome toolbar on forward SPA navigations."
    This reverts commit 891376a7b08afe2c6972f44024f01de98592fa7e.

--- components/navigation/BackNavigationCoordinator.tsx
+++ components/navigation/BackNavigationCoordinator.tsx
@@ -7,7 +7,6 @@ import {
   initBackNavigationDetection,
   notifyRouteSettled,
 } from "@/lib/navigation/back-navigation";
-import { resetMobileChromeToolbar } from "@/lib/navigation/reset-mobile-chrome-toolbar";
 import {
   getRouteUrl,
   restoreSavedRouteScroll,
@@ -24,18 +23,6 @@ export function BackNavigationCoordinator() {
   const pathname = usePathname();
   const searchParams = useSearchParams();
   const cancelRestoreRef = useRef<(() => void) | null>(null);
-  const isFirstPathnameRef = useRef(true);
-
-  useLayoutEffect(() => {
-    if (isFirstPathnameRef.current) {
-      isFirstPathnameRef.current = false;
-      return;
-    }
-
-    if (hasPendingBackNavigation()) return;
-
-    resetMobileChromeToolbar(0);
-  }, [pathname]);
 
   useLayoutEffect(() => {
     initBackNavigationDetection((event) => {
